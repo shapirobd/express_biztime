d98799c2c8eb50c980e0d1f0633bc66c59798021
Brian D. Shapiro <briand.shapiro@brians-mbp.attlocal.net>
2020-11-28 12:36:15 -0500

    POST /companies

--- routes/companies.js
+++ routes/companies.js
@@ -27,4 +27,17 @@ router.get("/:code", async (req, res, next) => {
 	}
 });
 
+router.post("/", async (req, res, next) => {
+	try {
+		const { code, name, description } = req.body;
+		const results = await db.query(
+			`INSERT INTO companies (code, name, description) VALUES ($1, $2, $3)`,
+			[code, name, description]
+		);
+		return res.status(201).json({ company: { code, name, description } });
+	} catch (e) {
+		next(e);
+	}
+});
+
 module.exports = router;
