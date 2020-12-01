11d85741dad4fb31ea5dbaefd04eef809d3e07c6
Brian D. Shapiro <briand.shapiro@Brians-MBP.attlocal.net>
2020-11-30 19:24:42 -0500

    all further study completed

--- routes/industries.js
+++ routes/industries.js
@@ -12,29 +12,29 @@ router.get("/", async (req, res, next) => {
 	}
 });
 
-// router.get("/:code", async (req, res, next) => {
-// 	try {
-// 		const { code } = req.params;
-// 		const results = await db.query(
-// 			`SELECT c.name, c.description, i.industry FROM companies AS c
-// 				LEFT JOIN companies_industries AS ci
-// 					ON ci.comp_code = c.code
-// 				LEFT JOIN industries AS i
-// 					ON ci.ind_code = i.code
-// 				WHERE c.code=$1`,
-// 			[code]
-// 		);
-// 		if (results.rows.length === 0) {
-// 			throw new ExpressError(`Can't find company with code ${code}`, 404);
-// 		}
-// 		const { name, description } = results.rows[0];
-// 		const company = { code, name, description };
-// 		company.industries = results.rows.map((r) => r.industry);
-// 		return res.json({ company: company });
-// 	} catch (e) {
-// 		next(e);
-// 	}
-// });
+router.get("/:code", async (req, res, next) => {
+	try {
+		const { code } = req.params;
+		const results = await db.query(
+			`SELECT i.industry, c.name FROM industries AS i
+				LEFT JOIN companies_industries AS ci
+					ON ci.ind_code = i.code
+				LEFT JOIN companies AS c
+					ON ci.comp_code = c.code
+				WHERE i.code=$1`,
+			[code]
+		);
+		if (results.rows.length === 0) {
+			throw new ExpressError(`Can't find industry with code ${code}`, 404);
+		}
+		const { name, industry } = results.rows[0];
+		const ind = { code, industry };
+		ind.companies = results.rows.map((r) => r.name);
+		return res.json({ industry: ind });
+	} catch (e) {
+		next(e);
+	}
+});
 
 router.post("/", async (req, res, next) => {
 	try {
